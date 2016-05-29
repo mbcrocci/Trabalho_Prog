1ced15795a89f1a8433ab23fc576fa48222900dd
g4rrucho <guilhermegarrucho@gmail.com>
2016-05-29 20:55:22 +0100

    Variables ncol and nrows are now in struct game_board.

--- new_game.c
+++ new_game.c
@@ -8,7 +8,7 @@ typedef struct game_board {
   int nrows, ncol;
 } Board;
 
-void set_rules(char *player1, char *player2,  int *nrows, int *ncol)
+void set_rules(char *player1, char *player2, Board *game_board)
 {
   char confirmation='n';
 
@@ -19,14 +19,14 @@ void set_rules(char *player1, char *player2,  int *nrows, int *ncol)
 
   do {
     printf("\t%s, insert the size of the game board.\nLines: ", player1);
-    scanf("%d", nrows);
+    scanf("%d", &(game_board->nrows));
     printf("Columns: ");
-    scanf("%d", ncol);
+    scanf("%d", &(game_board->ncol));
     getchar();
 
-    if(*ncol > *nrows) {
-      if(*nrows < 4 || *nrows > 8)
-        if(*ncol < 6 || *ncol > 10) {
+    if(game_board->ncol > game_board->nrows) {
+      if(game_board->nrows < 4 || game_board->nrows > 8)
+        if(game_board->ncol < 6 || game_board->ncol > 10) {
           printf("\nGame board size invalid! Number of columns and rows"
                   " invalid!\nPress any key to continue");
           getchar();
@@ -36,7 +36,7 @@ void set_rules(char *player1, char *player2,  int *nrows, int *ncol)
           getchar();
         }
       else
-          if(*ncol < 6 || *ncol > 10) {
+          if(game_board->ncol < 6 || game_board->ncol > 10) {
             printf("\nGame board size invalid! Number of columns invalid!\n"
                    "Press any key to continue");
             getchar();
@@ -52,44 +52,44 @@ void set_rules(char *player1, char *player2,  int *nrows, int *ncol)
       getchar();
     }
   } while((confirmation!='y' && confirmation!='Y')
-			|| *nrows < 4 || *nrows > 8 || *ncol < 6 || *ncol > 10
-      || *ncol < *nrows);
+			|| game_board->nrows < 4 || game_board->nrows > 8 || game_board->ncol < 6 || game_board->ncol > 10
+      || game_board->ncol < game_board->nrows);
 }
 
-void create_board(Board *game_board, int nrows, int ncol)
+void create_board(Board *game_board)
 {
   int i, j;
-  game_board->board = malloc(nrows * sizeof(char *));
-  for(i = 0;i < nrows; i++)
-    game_board->board[i] = malloc(ncol * sizeof(char));
+  game_board->board = malloc(game_board->nrows * sizeof(char *));
+  for(i = 0;i < game_board->nrows; i++)
+    game_board->board[i] = malloc(game_board->ncol * sizeof(char));
 
-  for(i = 0;i < nrows; i++)
-    for(j = 0;j < ncol; j++) {
+  for(i = 0;i < game_board->nrows; i++)
+    for(j = 0;j < game_board->ncol; j++) {
       game_board->board[i][j] = '*';
-      if(i == nrows - 1 && j == ncol - 1)
+      if(i == game_board->nrows - 1 && j == game_board->ncol - 1)
         game_board->board[i][j] = 'X';
     }
 
 }
 
-void free_board(Board *game_board, int nrows, int ncol)
+void free_board(Board *game_board)
 {
   int i;
-  for(i = 0; i < nrows; i++)
+  for(i = 0; i < game_board->nrows; i++)
     free(game_board->board[i]);
   free(game_board->board);
 }
 
-void print_board(Board game_board, int nrows, int ncol)
+void print_board(Board game_board)
 {
   int i, j;
 
   printf("\t\t|");
-  for(i = 0, j = 'A';i < ncol; i++)
+  for(i = 0, j = 'A';i < game_board.ncol; i++)
     printf(" %c |", j++);
 
-  for(i = 0;i < nrows; i++)
-    for(j = 0; j < ncol; j++) {
+  for(i = 0;i < game_board.nrows; i++)
+    for(j = 0; j < game_board.ncol; j++) {
       if(!j)
         printf("\n\t   | %2d |", i+1);
       printf(" %c |", game_board.board[i][j]);
@@ -99,14 +99,13 @@ void print_board(Board game_board, int nrows, int ncol)
 
 int get_pos(Board *game_board, int play_row, int play_col)
 {
-  //printf("cell[i][j]: %c\n", )
   return game_board->board[play_row][play_col];
-  //return *(*(game_board->board + play_row) + play_col);
 }
 
-int validation(Board *game_board, int nrows, int ncol, int play_row, int play_col, int *keep_play)
+int validation(Board *game_board, int play_row, int play_col, int *keep_play)
 {
-  if(play_row >= 0 && play_row < nrows && play_col >= 0 && play_col < ncol)
+  if(play_row >= 0 && play_row < game_board->nrows && play_col >= 0 &&
+     play_col < game_board->ncol)
     if(get_pos(game_board, play_row, play_col) == 'X') {
       *keep_play = 0;
       return 0;
@@ -119,7 +118,7 @@ int validation(Board *game_board, int nrows, int ncol, int play_row, int play_co
     return 1;
 }
 
-void play(Board *game_board, char *player, int nrows, int ncol, int *keep_play)
+void play(Board *game_board, char *player, int *keep_play)
 {
   int play_row, end_row, end_col, i, j;
   char play_col;
@@ -129,7 +128,7 @@ void play(Board *game_board, char *player, int nrows, int ncol, int *keep_play)
     scanf(" %c %d", &play_col, &play_row);
     end_row = play_row - 1;
     end_col = play_col - 'A';
-  } while(validation(game_board, nrows, ncol, end_row, end_col, keep_play));
+  } while(validation(game_board, end_row, end_col, keep_play));
 
   for(i = 0; i <= end_row; i++)
     for(j = 0; j <= end_col; j++)
@@ -142,16 +141,16 @@ void game()
 {
   Board game_board;
   char player1[NAME_STR_LEN], player2[NAME_STR_LEN], *curr_player = player1;
-  int nrows, ncol, keep_play = 1;
+  int keep_play = 1;
 
-  set_rules(player1, player2, &nrows, &ncol);
-  create_board(&game_board, nrows, ncol);
+  set_rules(player1, player2, &game_board);
+  create_board(&game_board);
 
-  print_board(game_board, nrows, ncol);
+  print_board(game_board);
   do {
 
-    play(&game_board, curr_player, nrows, ncol, &keep_play);
-    print_board(game_board, nrows, ncol);
+    play(&game_board, curr_player, &keep_play);
+    print_board(game_board);
 
     if(!strcmp(curr_player, player1))
       curr_player = player2;
@@ -160,7 +159,7 @@ void game()
   } while(keep_play);
   printf("%s, you won!\n", curr_player);
 
-  free_board(&game_board, nrows, ncol);
+  free_board(&game_board);
 }
 
 void menu()
